ba9df08d8bdedaadfff73efcb19bf6259b6ffd26
Darma Lopez <ldarma112@gmail.com>
2026-07-01 15:25:42 -0600

    Add home route

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 8080;
 
 const routes = require("./routes");
 
-app.use("/", routes);
+app.get("/", (req, res) => {
+  res.send("Contacts API is running!");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
