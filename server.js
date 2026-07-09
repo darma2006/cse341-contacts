ef8504a066a010c7623a4673ffb24a64fd500e28
Darma Lopez <ldarma112@gmail.com>
2026-07-09 11:25:55 -0600

    Added Swagger

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+
 app.use("/", routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
