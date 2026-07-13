f0d5e0a7b19ae8640c152b9417b958063840758f
Darma Lopez <ldarma112@gmail.com>
2026-07-13 10:16:19 -0600

    fixed week 2

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: "Contacts API",
     description: "CSE341 Contacts API"
   },
-  host: "localhost:8080",
+  host: "https://cse341-contacts-ta52.onrender.com",
   schemes: ["http"]
 };
 
